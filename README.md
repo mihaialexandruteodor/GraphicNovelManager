d8d965c279eb6fdced1e83ec621b80d2c9b8d99b
mihaialexandruteodor <mihaialexandruteodor@gmail.com>
2022-08-08 15:29:31 +0300

    added Tldraw canvas

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # GraphicNovelManager
+
+Leveraging the Tldraw Open Source npm library available here: 
+
+* https://github.com/tldraw/tldraw
+* https://www.npmjs.com/package/@tldraw/tldraw
--- graphic-novel-manager/src/App.js
+++ graphic-novel-manager/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Tldraw} from "@tldraw/tldraw";
 
 function App() {
   return (
     <>
     <div><h1><center>Text tool for comics</center></h1></div>
-    </>
+    <br/>
+    <div> <Tldraw
+        autofocus
+        disableAssets
+        showPages={false}
+      />
+    </div>
+  </>
   )
 }
 
